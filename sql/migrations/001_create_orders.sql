2cf4819abaf4c59c87cceb21cc2afc8d50ddabf7
DanhDik <thanhdanh1231102@gmail.com>
2026-07-30 13:01:10 +0700

    init table and fix something

--- sql/migrations/001_create_orders.sql
+++ sql/migrations/001_create_orders.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE IF NOT EXISTS orders (
+    order_id        TEXT            PRIMARY KEY,
+    event_id        UUID            NOT NULL    UNIQUE,
+    customer_id     TEXT            NOT NULL,
+    amount          NUMERIC(18, 2)  NOT NULL    CHECK (amount > 0),
+    currency        CHAR(3)         NOT NULL,
+    occurred_at     TIMESTAMP       NOT NULL,
+    ingested_at     TIMESTAMP       NOT NULL    DEFAULT NOW()
+)
--- sql/migrations/004_create_outbox.sql
+++ sql/migrations/004_create_outbox.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS outbox_events (
+    id              UUID        PRIMARY KEY,
+    aggregate_id    TEXT        NOT NULL,
+    event_type      TEXT        NOT NULL,
+    payload         JSONB       NOT NULL,
+    created_at      TIMESTAMP   NOT NULL    DEFAULT NOW(),
+    published_at    TIMESTAMP
+)
